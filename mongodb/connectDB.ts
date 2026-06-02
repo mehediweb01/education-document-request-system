fecdcfe330e880804340d659d9fc4df8b762d2ec
Md. Mehedi Hasan <mehedihasan0937580@gmail.com>
2026-06-02 18:01:15 +0600

    connectDB error

--- mongodb/connectDB.ts
+++ mongodb/connectDB.ts
@@ -5,11 +5,6 @@ export const connectDB = async (): Promise<void> => {
     await mongoose.connect(process.env.MONGO_URI as string);
     console.log("✅ Connected to database successfully");
   } catch (error: unknown) {
-    if (error instanceof Error) {
-      const errorSplit = error.message.split(":")[1];
-      console.log("❌ Mongodb connection error:", errorSplit.trim());
-    } else {
-      console.log("❌ Failed to connect to database");
-    }
+    console.error(`❌ Error connecting to database: ${error}`);
   }
 };
